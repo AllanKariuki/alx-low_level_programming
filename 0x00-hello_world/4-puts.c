acbdc87ab7299f3af446fe4fcfb69c2e515c7d85
AllanKariuki <alankariuki24@gmail.com>
2022-09-09 07:19:38 -0700

    Puts in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - prints a string
+ * Description: prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 
 int main(void)
 {

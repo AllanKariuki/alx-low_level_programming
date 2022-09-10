6d2df2fd4a4f3290988359d90b7e634b37c0c394
AllanKariuki <alankariuki24@gmail.com>
2022-09-10 06:45:11 -0700

    Printing letters of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase letters of the alphabet
+ * Description: Prints letter of the alphabet, a to z in lowercase
+ * Returns: 0
+ */
+
+int main(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z', ch++)
+		puthcar(ch);
+		putchar('\n');
+		getch();
+	return (0);
+}

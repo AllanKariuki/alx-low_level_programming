2e2ecbfaf9d5d7d2790f150597c4e3687211f8cf
AllanKariuki <alankariuki24@gmail.com>
2022-09-10 07:02:30 -0700

    Alphabets2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+nclude <stdio.h>
+
+/**
+ *
+ * main - prints alphabets
+ * Description: Prints letter of the alphabet, a to z in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch<= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}

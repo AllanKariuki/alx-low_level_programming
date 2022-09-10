e303a427e764466c8f68dcb9ba33a95496deb113
AllanKariuki <alankariuki24@gmail.com>
2022-09-10 08:12:19 -0700

    alphs excepts e & p

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets
+ * description: prints alphabets except p and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'p')
+			continue;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

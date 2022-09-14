4b9657571dd254373086cb5711be65e0d04c2268
Allan Kariuki <alankariuki24@gmail.com>
2022-09-14 17:41:09 +0300

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _islower - prints if true or false
+ * @c: the integer to check
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	int c;
+	int ch;
+	int val;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (c == ch)
+		{
+			val = 1;
+		}
+		else
+		{
+			val = 0;
+		}
+	}
+
+	return (val);
+}

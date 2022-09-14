a5f8b6d43efd36398572aaf7422ad8a35b50db24
Allan Kariuki <alankariuki24@gmail.com>
2022-09-14 18:50:19 +0300

    absolutes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value
+ * @int: the integer being checked
+ * Return: int
+ */
+
+int _abs(int)
+{
+	int abs;
+
+	if (int < 0)
+	{
+		abs = int * -1;
+	}else
+	{
+		abs = int;
+	}
+	return (abs);
+}

22c67cf0ebe905dd27eae52cf90d474202a3270c
Allan Kariuki <alankariuki24@gmail.com>
2022-09-14 18:15:35 +0300

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign and returns 1, 0, or -1
+ * @n: the integer being tested
+ * Return: 1, 0, -1
+ */
+
+int print_sign(int n)
+{
+	int val;
+
+	if (n < 0)
+		val = -1;
+		_putchar('-');
+	else if (n = 0)
+		val = 0;
+		_putchar('0');
+	else
+		val = 1;
+		_putchar('+');
+
+	return (val);
+}

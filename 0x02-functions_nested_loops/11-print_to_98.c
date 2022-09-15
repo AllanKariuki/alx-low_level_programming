0f32c6fcdf69a6c679c283a16b5d7bfb006cf03c
Allan Kariuki <alankariuki24@gmail.com>
2022-09-15 13:55:51 +0300

    Printing numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: the starting integer
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_putchar(i);
+		_putchar(',');
+		_putchar(' ');
+	}
+}

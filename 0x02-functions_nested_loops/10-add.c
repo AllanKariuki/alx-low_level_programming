2a019bd7443543f5cd569b92d6ce4cb2f3a4a31c
Allan Kariuki <alankariuki24@gmail.com>
2022-09-15 13:51:26 +0300

    Adding

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,8 @@
 
 /**
  * add - add two integers
+ * @a: integer being added to b
+ * @b: integer being added to a
  * Return: sum
  */
 
@@ -11,5 +13,5 @@ int add(int a, int b)
 
 	sum = a + b;
 
-	return(sum);
+	return (sum);
 }

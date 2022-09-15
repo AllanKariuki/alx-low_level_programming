9b38d3b68b01bf0169fdad526af070a019f21d5c
Allan Kariuki <alankariuki24@gmail.com>
2022-09-15 05:26:45 +0300

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,22 @@ void times_table(void)
 
 	for (r = 0; r <= 9; r++)
 	{
+		_putchar('0');
+
 		for (c = 0; c <= 9; c++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			res = r * c;
-			_putchar(res);
-			_putchar('\n');
+
+			if (res <= 9)
+				_putchar(' ');
+			else
+				_putchar((res / 10) + '0');
+
+			_putchar((res % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }

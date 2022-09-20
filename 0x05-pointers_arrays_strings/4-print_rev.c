adf40d1ba6c00b2b9a8007c245abc91459b0a586
AllanKariuki <alankariuki24@gmail.com>
2022-09-20 05:27:07 +0300

    print reversed strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a reversed string
+ * @s: string being reversed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int _strlen(char *s)
+	{
+		char *p = s;
+
+		while (*s)
+		{
+			s++;
+		}
+		return (s - p);
+	}
+
+	void print_rev(char *s)
+	{
+		int size = _strlen(s);
+
+		while (size--)
+		{
+			_putchar(*(s + size));
+		}
+
+		_putchar('\n');
+	}
+}

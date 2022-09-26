85b86f942e960841125fdfc5a922359e355cf773
AllanKariuki <alankariuki24@gmail.com>
2022-09-26 15:27:29 +0300

    Finding c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - returns a pointer to the first character c
+ * @s: pointer to a string
+ * @c: a character c
+ * Return: NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	c = 'c';
+
+	while(*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+
+	if (c == '\0')
+		return (s)
+
+	return (NULL)
+}

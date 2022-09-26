0efe09d84793e4735d0101eccb163cc6cba60308
AllanKariuki <alankariuki24@gmail.com>
2022-09-26 15:45:07 +0300

    Correction task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,17 +5,19 @@
  * @src: source pointer
  * @dest: destination pointer
  * @n: unsigned integer
- * Return: a pointer to @dest
+ * Return: a pointer to@dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+        unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
+        for (i = 0; i < n; i++)
+        {
+                dest[i] = src[i];
+        }
 
-	return(dest);
+        return (dest);
 }
+~
+

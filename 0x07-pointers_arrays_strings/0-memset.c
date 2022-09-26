390859a44e1c9e95878e601433bfeb45b432ac91
AllanKariuki <alankariuki24@gmail.com>
2022-09-26 14:48:04 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - fills pointer s with b for range n
+ * @s: pointer
+ * @b: value
+ * @n: integer
+ *Return: pointer s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for(i=0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+
+	return(s);
+}
